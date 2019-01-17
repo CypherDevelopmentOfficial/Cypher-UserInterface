2f9668ec625626c1bbc9135fc35d04d638373341
Team CypherDevelopment Remote Desktop <CyDevRD@john-stationairymacbook.local>
2019-01-17 16:45:29 +0100

    Some Initial work done (and I've finally added something to our HomeScreen!) Much work left though

---
+++ CypherUI/FeaturedBanner.swift
@@ -0,0 +1,27 @@
+//
+//  FeaturedBanner.swift
+//  CypherUI
+//
+//  Created by Team CypherDevelopment Remote Desktop on 17/01/2019.
+//  Copyright © 2019 CyDev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class FeaturedViewC: UIViewController {
+}
+class FeaturedView: UIView {
+    
+    
+    @IBOutlet weak var FeaturedName: UILabel!
+    @IBOutlet weak var PriceName: UILabel!
+    @IBOutlet weak var RepoSource: UILabel!
+    @IBOutlet weak var DeveloperName: UILabel!
+    @IBOutlet weak var FeaturedBannerImage: UIImageView!
+    @IBOutlet weak var Logo: UIImageView!
+    @IBAction func EnterInfo(_ sender: FeaturedView) {
+    
+    }
+
+    }
+
--- CypherUI/ThemesViewController.swift
+++ CypherUI/ThemesViewController.swift
@@ -10,18 +10,36 @@ import UIKit
 
 class ThemeViewController: UIViewController {
 
+    @IBOutlet weak var ThemesFeatureScrollView: UIScrollView!
+    let feature1 = ["DevelperName":"CypherDevelopment", "RepoSource": "YourDevice", "PriceName": "FREE", "FeaturedName": "Cypher", "FeaturedBannerImage": "BannerLaunch"]
+    let feature2 = ["DevelperName":"CypherDevelopment", "RepoSource": "YourDevice", "PriceName": "FREE", "FeaturedName": "Cypher", "FeaturedBannerImage": "BannerLaunch"]
+    var featureArray = [Dictionary<String,String>]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        featureArray = [feature1,feature2]
+        ThemesFeatureScrollView.isPagingEnabled = true
+        ThemesFeatureScrollView.contentSize = CGSize(width: self.view.bounds.width * CGFloat(featureArray.count), height:150)
+        ThemesFeatureScrollView.showsHorizontalScrollIndicator = false
+        
+     //   loadThemeFeatured()
+    }
+    
+    
+    //func loadThemeFeatured() {
+   //     for (index, featured) in featureArray.enumerated() {
+    //        let featureView = Bundle.main.loadNibNamed("Feature", owner: self, options: nil)?.first as? FeaturedView {}
+    //    }
+    //}
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
     }
 
-
-}
-class BasicThemePageLVL1: UIScrollView {
-    
-
-
-class BannerViewThemes: UIView {
-    
-}
 }
