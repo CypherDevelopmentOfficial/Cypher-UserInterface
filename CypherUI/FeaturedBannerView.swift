86eab5623c39fe9ccae5c96f14d768e371183767
Team CypherDevelopment Remote Desktop <CyDevRD@john-stationairymacbook.local>
2019-02-04 15:08:31 +0100

    Hooked all the points up... and then i remembered... I could also just make it work with a couple lines of code.... too late anyways

--- CypherUI/FeaturedBannerView.swift
+++ CypherUI/FeaturedBannerView.swift
@@ -18,6 +18,97 @@ class FeaturedBannerView: UIView {
 }
 
 
-class TopShelfScrollView: UIView {
-    
+class TopSpotScrollView: UIView {
+    // This is a mess, i know. i hope i learn to clean this one day
+    // **IMPORTANT: L stands for Logo. N stands for logo. P stands for Price. R stands for Rating (x/5). D stands for Developer
+    //  #1
+    @IBOutlet weak var L1: UIImageView!
+    @IBOutlet weak var N1: UILabel!
+    @IBOutlet weak var P1: UILabel!
+    @IBOutlet weak var R1: UILabel!
+    @IBOutlet weak var D1: UILabel!
+    // #2
+    @IBOutlet weak var L2: UIImageView!
+    @IBOutlet weak var N2: UILabel!
+    @IBOutlet weak var P2: UILabel!
+    @IBOutlet weak var R2: UILabel!
+    @IBOutlet weak var D2: UILabel!
+    // #3
+    @IBOutlet weak var L3: UIImageView!
+    @IBOutlet weak var N3: UILabel!
+    @IBOutlet weak var P3: UILabel!
+    @IBOutlet weak var R3: UILabel!
+    @IBOutlet weak var D3: UILabel!
+    // #4
+    @IBOutlet weak var L4: UIImageView!
+    @IBOutlet weak var N4: UILabel!
+    @IBOutlet weak var P4: UILabel!
+    @IBOutlet weak var R4: UILabel!
+    @IBOutlet weak var D4: UILabel!
+    // #5
+    @IBOutlet weak var L5: UIImageView!
+    @IBOutlet weak var N5: UILabel!
+    @IBOutlet weak var P5: UILabel!
+    @IBOutlet weak var R5: UILabel!
+    @IBOutlet weak var D5: UILabel!
+    // #6
+    @IBOutlet weak var L6: UIImageView!
+    @IBOutlet weak var N6: UILabel!
+    @IBOutlet weak var P6: UILabel!
+    @IBOutlet weak var R6: UILabel!
+    @IBOutlet weak var D6: UILabel!
+    // #7
+    @IBOutlet weak var L7: UIImageView!
+    @IBOutlet weak var N7: UILabel!
+    @IBOutlet weak var P7: UILabel!
+    @IBOutlet weak var R7: UILabel!
+    @IBOutlet weak var D7: UILabel!
+    // #8
+    @IBOutlet weak var L8: UIImageView!
+    @IBOutlet weak var N8: UILabel!
+    @IBOutlet weak var P8: UILabel!
+    @IBOutlet weak var R8: UILabel!
+    @IBOutlet weak var D8: UILabel!
+    // #9
+    @IBOutlet weak var L9: UIImageView!
+    @IBOutlet weak var N9: UILabel!
+    @IBOutlet weak var P9: UILabel!
+    @IBOutlet weak var R9: UILabel!
+    @IBOutlet weak var D9: UILabel!
+    // #10
+    @IBOutlet weak var L10: UIImageView!
+    @IBOutlet weak var N10: UILabel!
+    @IBOutlet weak var P10: UILabel!
+    @IBOutlet weak var R10: UILabel!
+    @IBOutlet weak var D10: UILabel!
+    // #11
+    @IBOutlet weak var L11: UIImageView!
+    @IBOutlet weak var N11: UILabel!
+    @IBOutlet weak var P11: UILabel!
+    @IBOutlet weak var R11: UILabel!
+    @IBOutlet weak var D11: UILabel!
+    // #12
+    @IBOutlet weak var L12: UIImageView!
+    @IBOutlet weak var N12: UILabel!
+    @IBOutlet weak var P12: UILabel!
+    @IBOutlet weak var R12: UILabel!
+    @IBOutlet weak var D12: UILabel!
+    // #13
+    @IBOutlet weak var L13: UIImageView!
+    @IBOutlet weak var N13: UILabel!
+    @IBOutlet weak var P13: UILabel!
+    @IBOutlet weak var R13: UILabel!
+    @IBOutlet weak var D13: UILabel!
+    // #14
+    @IBOutlet weak var L14: UIImageView!
+    @IBOutlet weak var N14: UILabel!
+    @IBOutlet weak var P14: UILabel!
+    @IBOutlet weak var R14: UILabel!
+    @IBOutlet weak var D14: UILabel!
+    // #15
+    @IBOutlet weak var L15: UIImageView!
+    @IBOutlet weak var N15: UILabel!
+    @IBOutlet weak var P15: UILabel!
+    @IBOutlet weak var R15: UILabel!
+    @IBOutlet weak var D15: UILabel!
 }
